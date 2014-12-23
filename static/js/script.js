aa3db10c1435dc88fd718d7f7ce40cfc1f368eb7
Pillows <pillows.hf@gmail.com>
2014-12-22 21:48:18 -0500

    PLease let me finish

--- static/js/script.js
+++ static/js/script.js
@@ -14,20 +14,11 @@ $(document).ready(function() {
             keyboard:true
         })
     });
-    /* End Upload Window */
     
-    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
-        
-        var input = $(this).parents('.input-group').find(':text'),
-            log = numFiles > 1 ? numFiles + ' files selected' : label;
-        
-        if( input.length ) {
-            input.val(log);
-        } else {
-            if( log ) alert(log);
-        }
-        
+    $('.file-upload').click(function() {
+	    $('.modal-body').html("");
     });
+    /* End Upload Window */
     
     /* WebM Stats */
     $("[data-toggle=tooltip]").tooltip();
